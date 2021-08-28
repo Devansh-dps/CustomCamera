6c7aa343189207bdfe14ad79668c7cc726628319
Devansh-dps <60577720+Devansh-dps@users.noreply.github.com>
2021-08-28 12:35:19 +0530

    Add files via upload

---
+++ CustomCamera/CustomCamera/PhotoViewController.swift
@@ -0,0 +1,45 @@
+//
+//  PhotoViewController.swift
+//  CustomCamera
+//
+//  Created by DEVANSH PRATAP SINGH on 04/03/20.
+//  Copyright © 2020 DEVANSH PRATAP SINGH. All rights reserved.
+//
+
+import UIKit
+
+class PhotoViewController: UIViewController {
+    
+    var takenPhoto:UIImage?
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        if let availableImage = takenPhoto {
+            imageView.image = availableImage
+    }
+    }
+    
+    @IBAction func goBack(_ sender: Any) {
+        
+        self.dismiss(animated: true, completion: nil)
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning ()
+        
+        }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
